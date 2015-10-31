ebde4dacf798efb6112d7700a95a05e97c3cbf9b
srzmldl <ldlong@mail.ustc.edu.cn>
2015-11-01 00:58:20 +0800

    finish lesson4

--- Lesson3/bashI.sh
+++ Lesson3/bashI.sh
@@ -1,8 +1,8 @@
-while true; do
-    ./makeI
-    ./I
-    ./checkI
-    if diff iq.out iqstd.out; then
+while true; do   # 注释
+    ./makeI    # 运行数据生成器
+    ./I        #运行要提交的程序
+    ./checkI   # 运行暴力检查程序
+    if diff iq.out iqstd.out; then # 比较
         echo "AC"
     else
         echo "WA"
--- Lesson3/makeI.cpp
+++ Lesson3/makeI.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int main()<
+int main()
 {
     srand(clock() % time(0));
     freopen("iq.in", "w", stdout);
